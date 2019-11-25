7d9c291f26f625686808981386895eddc87fcaf6
afifpratama <afifpratama@gmail.com>
2019-11-25 15:19:18 +0700

    Updates

---
+++ precache-manifest.837acbfec143d2d51679e4f968d6f460.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909d9cfe5de20c2e7754236e30b28856",
+    "url": "/latihan-react/index.html"
+  },
+  {
+    "revision": "858ff2baecd43ee7f125",
+    "url": "/latihan-react/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "4ef3ed8f985d77fc269a",
+    "url": "/latihan-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "858ff2baecd43ee7f125",
+    "url": "/latihan-react/static/js/2.520ff597.chunk.js"
+  },
+  {
+    "revision": "4ef3ed8f985d77fc269a",
+    "url": "/latihan-react/static/js/main.4e13563d.chunk.js"
+  },
+  {
+    "revision": "cbf13abc49a6b023acc1",
+    "url": "/latihan-react/static/js/runtime-main.4fca9940.js"
+  }
+]);
