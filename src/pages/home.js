84f3922339fb1abee6d8004ec2c927967bdff67b
afifpratama <afifpratama@gmail.com>
2019-11-26 11:02:14 +0700

    fake api

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,51 @@
 import React, { Component } from "react"
+import Axios from "axios"
+import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
 
 class Home extends Component {
+
+    state = {
+        data: []
+    }
+
+    componentDidMount() {
+        Axios.get("http://localhost:2000/users")
+        .then((res) => {
+            this.setState({ data: res.data })
+            console.log(this.state.data)
+        })
+        .catch((err) => {
+
+        })
+    }
+
+    contoh = () => {
+
+    }
+
+    renderUSerData = () => {
+        return this.state.data.map((val) => {
+            return (
+                <Row>
+                    <Col sm="6">
+                        <Card body>
+                            <CardTitle>{val.first_name}</CardTitle>
+                            <CardText>{val.last_name}</CardText>
+                            <Button>{val.email}</Button>
+                        </Card>
+                    </Col>
+                </Row>
+            )
+        })
+    }
+
     render() {
         return (
             <div>
                 ini Home
-            </div>
+                <button onClick={this.contoh}> click me</button>
+                {this.renderUSerData()}
+            </div >
         )
     }
 }
